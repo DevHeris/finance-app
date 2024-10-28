1f2f6f24178257c3fd733e270ad737b4edf4f558
DevHeris <badairoinioluwa578@gmail.com>
2024-10-28 11:58:00 +0100

    UPDATE

--- src/app/pages/overview/overview-transaction-list-item/overview-transaction-list-item.component.ts
+++ src/app/pages/overview/overview-transaction-list-item/overview-transaction-list-item.component.ts
@@ -11,4 +11,12 @@ export class OverviewTransactionListItemComponent {
   // SENDER == WHO SENT YOU THE MONEY?
 
   transaction = input<Transaction>();
+
+  formatDate(value: any) {
+    let date = new Date(value);
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    return day + ' ' + month + ', ' + year;
+  }
 }
