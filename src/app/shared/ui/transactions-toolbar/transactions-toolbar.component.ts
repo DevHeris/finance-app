88819f68c3e65d99b1765ed3aaacc015cbf7a768
DevHeris <badairoinioluwa578@gmail.com>
2024-10-29 20:28:32 +0100

    DROPDOWN ON MOBILE

--- src/app/shared/ui/transactions-toolbar/transactions-toolbar.component.ts
+++ src/app/shared/ui/transactions-toolbar/transactions-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-transactions-toolbar',
@@ -6,11 +6,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrl: './transactions-toolbar.component.css',
   encapsulation: ViewEncapsulation.None,
 })
-export class TransactionsToolbarComponent {
-  categorySelected: string = 'All Transactions';
-  sortOptionSelected: string = 'Latest';
-  sortByOptions: string[] = ['Latest', 'Oldest', 'A to Z', 'Z to A', 'Highest', 'Lowest'];
-  categoryOptions: string[] = [
+export class TransactionsToolbarComponent implements AfterViewInit {
+  isSortOpen = false;
+  isCategoryOpen = false;
+  dropdownStyle = { top: '0px', right: '0px' };
+
+  sortOptionSelected = 'Latest';
+  categorySelected = 'All Transactions';
+
+  sortByOptions = ['Latest', 'Oldest', 'A to Z', 'Z to A', 'Highest', 'Lowest'];
+  categoryOptions = [
     'All Transactions',
     'Dining Out',
     'General',
@@ -23,4 +28,38 @@ export class TransactionsToolbarComponent {
     'Bills',
     'Shopping',
   ];
+
+  @ViewChild('sortIcon', { static: false }) sortIcon!: ElementRef;
+  @ViewChild('categoryIcon', { static: false }) categoryIcon!: ElementRef;
+
+  ngAfterViewInit() {}
+
+  toggleDropdown(type: 'sort' | 'category') {
+    const targetIcon = type === 'sort' ? this.sortIcon : this.categoryIcon;
+    const rect = targetIcon.nativeElement.getBoundingClientRect();
+
+    this.dropdownStyle = {
+      top: `${rect.bottom + window.scrollY + 30}px`,
+      //   right: `${rect.right}px`,
+      right: '4.2%',
+    };
+
+    if (type === 'sort') {
+      this.isSortOpen = !this.isSortOpen;
+      this.isCategoryOpen = false;
+    } else {
+      this.isCategoryOpen = !this.isCategoryOpen;
+      this.isSortOpen = false;
+    }
+  }
+
+  handleSelection(option: string) {
+    if (this.isSortOpen) {
+      this.sortOptionSelected = option;
+    } else {
+      this.categorySelected = option;
+    }
+    this.isSortOpen = false;
+    this.isCategoryOpen = false;
+  }
 }
